e92a30e1872b6be138d13c3fcccc2d89eda5c58f
bhuiamhe <bhuiamh@gmail.com>
2023-05-21 01:51:54 +0600

    MongoDB problem Solved

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.axhfmt5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,33 +20,16 @@ const client = new MongoClient(uri, {
   },
 });
 
-const verifyJWT = (req, res, next) => {
-  console.log("heating jwt");
-  console.log(req.headers.authorization);
-  const authorization = req.headers.authorization;
-  if (!authorization) {
-    return res
-      .status(401)
-      .send({ error: true, message: "unauthorized Access" });
-  }
-
-  const token = authorization.spilt(" ")[1];
-  console.log("token", token);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decode) => {
-    if (error) {
-      return res
-        .status(403)
-        .send({ error: true, message: "Unauthorize Access" });
-    }
-  });
-};
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db("theToyLab").collection("alltoys");
+    app.get("/alltoys", async (req, res) => {
+      const result = await toysCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -54,9 +37,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
@@ -66,3 +50,62 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`The Toy Lab Server is running on port ${port}`);
 });
+
+// Use Latter
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.axhfmt5.mongodb.net/?retryWrites=true&w=majority`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// const verifyJWT = (req, res, next) => {
+//   console.log("heating jwt");
+//   console.log(req.headers.authorization);
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res
+//       .status(401)
+//       .send({ error: true, message: "unauthorized Access" });
+//   }
+
+//   const token = authorization.spilt(" ")[1];
+//   console.log("token", token);
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decode) => {
+//     if (error) {
+//       return res
+//         .status(403)
+//         .send({ error: true, message: "Unauthorize Access" });
+//     }
+//   });
+// };
+
+// // const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//     const toysCollection = client.db("theToyLab").collection("alltoys");
+//     app.get("/alltoys", async (res, req) => {
+//       const result = await toysCollection.find().toArray();
+//       res.send(result);
+//     });
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
