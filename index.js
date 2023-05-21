030a28f3ba8df1eab33850a5c34fcc4a9d108f36
bhuiamhe <bhuiamh@gmail.com>
2023-05-21 14:08:09 +0600

    Databage add for Get Operation

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.axhfmt5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,10 +26,20 @@ async function run() {
     await client.connect();
 
     const toysCollection = client.db("theToyLab").collection("alltoys");
+
     app.get("/alltoys", async (req, res) => {
       const result = await toysCollection.find().toArray();
       res.send(result);
     });
+
+    app.get("/alltoys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
